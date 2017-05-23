bc7a04374a739f7e8da2b0d451b26437353b8a3c
zluo16 <zluo@alumni.berklee.edu>
2017-05-23 12:26:51 -0400

    poke scraper - i tried

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
+require "pry"
+
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(table)
+    @id = table[:id]
+    @name = table[:name]
+    @type = table[:type]
+    @db = table[:db]
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (id, name, type) VALUES
+    (?, ?, ?)", 1, name, type)
+  end
+
+  def self.find(id, db)
+    new_poke = db.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
+    poke_hash = {}
+    poke_hash[:id] = new_poke[0]
+    poke_hash[:name] = new_poke[1]
+    poke_hash[:type] = new_poke[2]
+    poke_hash[:db] = db
+    self.new(poke_hash)
+  end
+
 end
